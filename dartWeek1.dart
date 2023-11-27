f5fb0d8cd1cb5a5db1ff53061a65fe42d680ddbc
AdithyaAjeesh <143420297+AdithyaAjeesh@users.noreply.github.com>
2023-11-27 11:35:40 +0530

    Add files via upload

---
+++ dartWeek1.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+// // odd or even
+//   stdout.write("Enter a number: ");
+//   int userNumber = int.parse(stdin.readLineSync()!);
+//   if (userNumber % 2 == 0) {
+//     print("number is even");
+//   } else {
+//     print("number is odd");
+//   }
+
+  // //number less than 5
+  // List<int> arr = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  // for (var element in arr) {
+  //   if (element < 5) {
+  //     print(element);
+  //   }
+  // }
+
+// // pattern program
+//   int num = 3;
+//   int k = 1;
+//   for (var i = 1; i <= num; i++) {
+//     for (var j = 1; j <= i; j++) {
+//       stdout.write(k);
+//       k++;
+//     }
+//     stdout.writeln();
+//   }
+
+// // multiplication of 5
+//  int num = 5;
+//   for (var i = 1; i <= 10; i++) {
+//     print(num * i);
+//   }
+
+// // remove all white space
+//   print("Enter a string with whitespaces:");
+//   String inputString = stdin.readLineSync()!;
+//   String stringWithoutSpaces = inputString.replaceAll(' ', '');
+//   print("String without whitespaces: $stringWithoutSpaces");
+
+// //decending order in array
+//   List<int> inputList = [20, 10, 50, 30, 40];
+//   inputList.sort((a, b) => b.compareTo(a));
+//   print(inputList);
+
+//power of certain number
+  // int base = 5;
+  // int exponent = 3;
+  // double result = pow(base, exponent).toDouble();
+  // print("$base^$exponent = $result");
+
+// //get ramdom password
+//   int passwordLength = 8;
+//   String randomPassword = generateRandomPassword(passwordLength);
+//   print("Generated Password: $randomPassword");
+// }
+
+// String generateRandomPassword$ dnf module list(int length) {
+//   const String charset =
+//       'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+//   Random random = Random();
+//   List<String> passwordList =  List.generate(length, (index) => charset[random.nextInt(charset.length)]);
+//   return passwordList.join();
+
+}
